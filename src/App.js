30126b471d9cb69b4944d157bdbdc90178562b52
rockwoodc <rockwoodce@gmail.com>
2022-10-04 22:33:20 -0400

    contact form synced with state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
-import About from './components/About';
-import Nav from './components/Nav'
-import Gallery from './components/Gallery';
+import About from "./components/About";
+import Nav from "./components/Nav";
+import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
 
 function App() {
   const [categories] = useState([
     {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
     },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
@@ -25,7 +30,8 @@ function App() {
       ></Nav>
       <main>
         <div>
-        <Gallery currentCategory={currentCategory}></Gallery>
+          <ContactForm></ContactForm>
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
         </div>
       </main>
